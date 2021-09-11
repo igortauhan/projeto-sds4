eef9f1443957760521422379071f97de79f11aa7
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-09-11 15:45:39 -0300

    Layers

---
+++ backend/src/main/java/com/igort/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,8 @@
+package com.igort.dsvendas.repositories;
+
+import com.igort.dsvendas.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
---
+++ backend/src/main/java/com/igort/dsvendas/service/SellerService.java
@@ -0,0 +1,22 @@
+package com.igort.dsvendas.service;
+
+import com.igort.dsvendas.dto.SellerDTO;
+import com.igort.dsvendas.entities.Seller;
+import com.igort.dsvendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SellerService {
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    public List<SellerDTO> findAll() {
+        List<Seller> result = sellerRepository.findAll();
+        return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+    }
+}
